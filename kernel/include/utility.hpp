207abf9d34a7cf5d821d9d12603ccaf8fef6b5a0
Adam Kliś <root@bonusplay.pl>
2021-08-21 02:45:30 +0200

    add memdump helper

---
+++ kernel/include/utility.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <dstd/cstdint.hpp>
+
+void dump_memory(uint8_t* addr_from, uint8_t* addr_to);
---
+++ kernel/src/utility.cpp
@@ -0,0 +1,17 @@
+#include "utility.hpp"
+#include "serial.hpp"
+
+void dump_memory(uint8_t* addr_from, uint8_t* addr_to)
+{
+    for (uint8_t* addr_curr = addr_from; addr_curr < addr_to; ++addr_curr)
+    {
+        if (reinterpret_cast<uint64_t>(addr_curr) % 16 == 0)
+        {
+            serial::print("\n");
+            serial::print(dstd::addr_to_string(addr_curr));
+            serial::print(" ");
+        }
+        serial::print(dstd::to_string(*addr_curr, 16));
+        serial::print(" ");
+    }
+}
